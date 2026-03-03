7d132b6e27d5d650c738e6ca567dc619f567283a
Your Name <aldobesma@gmail.com>
2026-03-03 08:17:49 +0700

    update

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trello Lite API</title>
+    <title>Trello Lite API update ci cd</title>
     <style>
         :root {
             --bg: #0f172a;
